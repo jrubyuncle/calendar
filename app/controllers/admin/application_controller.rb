e581b14d74f236d4489ff97a26de5b3721713b94
odin1in <odin@pwn.so>
2015-05-22 22:36:53 +0800

    add admin guard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,10 @@
 module Admin
   class ApplicationController < ::ApplicationController
+    before_action :authenticate_admin_user!
 
+    private
+      def authenticate_admin_user!
+        render text: "Forbidden", status: 403 unless current_user.admin?
+      end
   end
 end
