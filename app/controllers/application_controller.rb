d56f7a879d590ac9d1ea556dbe05cb713f90f732
odin1in <odin@pwn.so>
2015-05-23 10:36:14 +0800

    add authenticate user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :user_signed_in?
 
+  def authenticate_user!
+    redirect_to sign_in_path unless user_signed_in?
+  end
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
